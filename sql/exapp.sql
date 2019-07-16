6c2fc651a26fbe0e942f0086f8e9457194fc87c9
Rahul Pawar <rahulpwar654@gmail.com>
2019-07-16 15:20:41 +0530

    update DB with student table

--- sql/exapp.sql
+++ sql/exapp.sql
@@ -1,54 +1,115 @@
-CREATE DATABASE  IF NOT EXISTS `exapp` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `exapp`;
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: exapp
--- ------------------------------------------------------
--- Server version	5.7.17
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2019 at 11:49 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exapp`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `bacons`
 --
 
-DROP TABLE IF EXISTS `bacons`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bacons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `type` varchar(45) NOT NULL,
-  `price` decimal(10,0) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `price` decimal(10,0) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `bacons`
 --
 
-LOCK TABLES `bacons` WRITE;
-/*!40000 ALTER TABLE `bacons` DISABLE KEYS */;
-INSERT INTO `bacons` VALUES (1,'chunky',24.5),(4,'slim',12.98),(5,'fatty',36.6),(6,'extra chunky',25),(7,'tender',25),(8,'smoky',25),(9,'striped',26.98),(10,'musky',25);
-/*!40000 ALTER TABLE `bacons` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `bacons` (`id`, `type`, `price`) VALUES
+(1, 'musky', '123'),
+(4, 'slim', '13'),
+(5, 'fatty', '37'),
+(6, 'extra chunky', '25'),
+(7, 'tender', '25'),
+(8, 'smoky', '25'),
+(9, 'striped', '27'),
+(10, 'musky', '25'),
+(11, 'truffy', '99'),
+(12, 'Pixel', '20'),
+(13, 'BadWay', '23'),
+(14, 'truffy', '99'),
+(15, 'truffy', '99'),
+(16, 'Samual', '45');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `collegeid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `firstname`, `lastname`, `collegeid`) VALUES
+(1, 'Rahul', 'Pawar', 20),
+(2, 'Yash', 'Tiwari', 20),
+(3, 'Vaibhav', 'Salagare', 20),
+(4, 'Mayank', 'Gupta', 20);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bacons`
+--
+ALTER TABLE `bacons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bacons`
+--
+ALTER TABLE `bacons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-05-24 23:17:35
