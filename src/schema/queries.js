e8050dbe8727f09a24d6318d7eff966641599089
Rahul Pawar <rahulpwar654@gmail.com>
2019-07-23 12:43:49 +0530

    Composite module

--- src/schema/queries.js
+++ src/schema/queries.js
@@ -1,8 +1,8 @@
 const { GraphQLObjectType } = require('graphql')
 const baconQueries = require('../model/bacon/queries')
 const studentQueries = require('../model/student/queries')
-
-
+const compQueries = require('../model/composite/queries')
+const collegeQueries = require('../model/college/queries')
 
 module.exports = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -14,7 +14,14 @@ module.exports = new GraphQLObjectType({
         getBecon: baconQueries.getAllBecon,
         //for student
         getStudents:studentQueries.getStudent,
-        getStudentByID: studentQueries.getStudentByID
+        getStudentByID: studentQueries.getStudentByID,
+
+        //For College
+        colleges:collegeQueries.colleges,
+        getCollegeById:collegeQueries.getCollegeById,
+
+        //Composite
+        getStudBecon: compQueries.getStudBecon
 
     }
 })
